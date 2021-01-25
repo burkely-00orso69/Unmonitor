2bf38bd37f97bc253c551d880ad643d9bd09a59c
dmintz7 <dmintz37@gmail.com>
2021-01-25 11:21:09 -0500

    Change Name

--- Unmonitor.py
+++ Unmonitor.py
@@ -3,8 +3,8 @@ from logging.handlers import RotatingFileHandler
 from plexapi.server import PlexServer
 from datetime import datetime, timedelta
 
-print(os.path.dirname(os.path.abspath(__file__)).split("/")[-1])
-extra = {'folder_name': os.path.dirname(os.path.abspath(__file__)).split("/")[-1]}
+filename, file_extension = os.path.splitext(os.path.basename(__file__))
+formatter = logging.Formatter('%(asctime)s - %(levelname)10s - %(module)15s:%(funcName)30s:%(lineno)5s - %(message)s')
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 consoleHandler = logging.StreamHandler(sys.stdout)
@@ -12,11 +12,9 @@ consoleHandler.setFormatter(formatter)
 logger.addHandler(consoleHandler)
 logging.getLogger("requests").setLevel(logging.WARNING)
 logger.setLevel(config.LOG_LEVEL)
-fileHandler = RotatingFileHandler(config.LOG_FOLDER + '/Unmonitor.log', maxBytes=1024 * 1024 * 1, backupCount=1)
+fileHandler = RotatingFileHandler(config.LOG_FOLDER + '/' + filename + '.log', maxBytes=1024 * 1024 * 1, backupCount=1)
 fileHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
-logger = logging.LoggerAdapter(logger, extra)
-
 
 show_user_list = config.unmonitor_shows_users
 
